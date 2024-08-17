a149d85b760e40280d372d35554a146ac4f1e2d4
nirmaldhakal2003 <nirmalbsccsit2@gmail.com>
2024-08-17 21:29:12 +0545

    day 3 ko content hun

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+
+app.set('view engine', 'ejs')
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send("Hello world")
-    
+    const data = {
+        name: "Nirmal Dhakal",
+        age: 21,
+        location: "Rampur"
+    }
+    res.render("home.ejs", {
+        home: data
+    })
 })
- app.get('/about', (req, res) =>{
+
+app.get('/about', (req, res) => {
     res.send("this is about page")
- })
-app.listen(3000,()=>{
-    console.log('Node js has been started: ')
+})
+
+
+app.use(express.static('public/css/'))
+
+
+app.listen(3000, () => {
+    console.log('Node.js has been started:')
 })
